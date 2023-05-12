3bcb8f63bf454160b1180d7c24b0f33dff1a5c94
Wilfred Nankpak Bale <nankpakbalewilfred@gmail.com>
2023-05-12 02:58:31 +0100

    0-subs.py

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,12 @@
+import requests
+
+def number_of_subscribers(subreddit):
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
+        return 0
+    data = response.json().get('data')
+    if not data:
+        return 0
+    return data.get('subscribers', 0)
